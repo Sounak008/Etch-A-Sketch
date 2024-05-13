363c25b028498022cbe69a56d15d6b117c239038
Sounak008 <sounaks706@gmail.com>
2024-05-13 16:42:33 +0530

    Added Prompt System

--- javascript.js
+++ javascript.js
@@ -1,14 +1,34 @@
 let can = document.querySelector('#canvas');
 let gridBox;
-for(let i = 1; i <= 16*16; i++){
+let boxes = 16;
+
+
+let button = document.getElementById('btn');
+function promptFunction() {
+    boxes = Number(prompt("Enter square size", "16"));
+    createGrid();
+}
+
+button.onclick = promptFunction;
+
+function createGrid() {
+
+can.innerHTML = ' ';
+
+for(let i = 1; i <= boxes*boxes; i++){
 gridBox = document.createElement('div');
 can.appendChild(gridBox);
 gridBox.classList.add('gridBox');
+gridBox.style.width = `calc(100%/${boxes})`;
+gridBox.style.height = `calc(100%/${boxes})`;
 }
 
+
 let gridBoxHover = document.querySelectorAll('.gridBox')
-gridBoxHover.forEach((gridBox) => 
+gridBoxHover.forEach((gridBox) =>
     gridBox.addEventListener('mouseover', () => {
         gridBox.classList.add('gridHover');
 })
 )
+}
+createGrid();
