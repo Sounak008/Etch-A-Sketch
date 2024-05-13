062a1568f145b4e4be55be055a791adf435325fb
Sounak008 <sounaks706@gmail.com>
2024-05-13 16:43:01 +0530

    Update Prompt System

--- javascript.js
+++ javascript.js
@@ -1,34 +1,31 @@
-let can = document.querySelector('#canvas');
+let can = document.querySelector("#canvas");
 let gridBox;
 let boxes = 16;
 
-
-let button = document.getElementById('btn');
+let button = document.getElementById("btn");
 function promptFunction() {
-    boxes = Number(prompt("Enter square size", "16"));
-    createGrid();
+  boxes = Number(prompt("Enter square size", "16"));
+  createGrid();
 }
 
 button.onclick = promptFunction;
 
 function createGrid() {
+  can.innerHTML = " ";
 
-can.innerHTML = ' ';
+  for (let i = 1; i <= boxes * boxes; i++) {
+    gridBox = document.createElement("div");
+    can.appendChild(gridBox);
+    gridBox.classList.add("gridBox");
+    gridBox.style.width = `calc(100%/${boxes})`;
+    gridBox.style.height = `calc(100%/${boxes})`;
+  }
 
-for(let i = 1; i <= boxes*boxes; i++){
-gridBox = document.createElement('div');
-can.appendChild(gridBox);
-gridBox.classList.add('gridBox');
-gridBox.style.width = `calc(100%/${boxes})`;
-gridBox.style.height = `calc(100%/${boxes})`;
-}
-
-
-let gridBoxHover = document.querySelectorAll('.gridBox')
-gridBoxHover.forEach((gridBox) =>
-    gridBox.addEventListener('mouseover', () => {
-        gridBox.classList.add('gridHover');
-})
-)
+  let gridBoxHover = document.querySelectorAll(".gridBox");
+  gridBoxHover.forEach((gridBox) =>
+    gridBox.addEventListener("mouseover", () => {
+      gridBox.classList.add("gridHover");
+    })
+  );
 }
 createGrid();
